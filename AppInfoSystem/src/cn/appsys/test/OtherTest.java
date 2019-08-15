3d98ec112c80188f39206c96db0b691dc14ba212
571327732 <571327732@qq.com>
2019-08-15 16:43:36 +0800

    add Test.java and OtherTest.java

---
+++ AppInfoSystem/src/cn/appsys/test/OtherTest.java
@@ -0,0 +1,9 @@
+package cn.appsys.test;
+
+public class OtherTest {
+
+	public static void main(String[] args) {
+		System.out.println("OtherTest main()");
+		System.out.println("OtherTest.java");
+	}
+}
---
+++ AppInfoSystem/src/cn/appsys/test/Test.java
@@ -0,0 +1,9 @@
+package cn.appsys.test;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Test main()");
+		System.out.println("Test.java");
+	}
+
+}
