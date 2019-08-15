cf4a514062f559386ebb50bfad8bb18fea98f4ff
571327732 <571327732@qq.com>
2019-08-15 16:46:07 +0800

    update OtherTest.java

--- AppInfoSystem/src/cn/appsys/test/OtherTest.java
+++ AppInfoSystem/src/cn/appsys/test/OtherTest.java
@@ -5,5 +5,6 @@ public class OtherTest {
 	public static void main(String[] args) {
 		System.out.println("OtherTest main()");
 		System.out.println("OtherTest.java");
+		System.out.println("测试");
 	}
 }
